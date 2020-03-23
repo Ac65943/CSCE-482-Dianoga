e39185ad73baa41462657cf6c709bd385534be53
Ac65943 <55357795+Ac65943@users.noreply.github.com>
2020-03-23 13:54:54 -0500

    simple_upload

--- datasite/data/views.py
+++ datasite/data/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the data index.")
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name,myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request,'core/simple_upload')
