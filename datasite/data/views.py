67a899f740eb5d8738e12c84425b203f71b3b8f8
Ac65943 <55357795+Ac65943@users.noreply.github.com>
2020-03-23 15:17:24 -0500

    Revert "simple_upload"
    This reverts commit e39185ad73baa41462657cf6c709bd385534be53.

--- datasite/data/views.py
+++ datasite/data/views.py
@@ -1,12 +1,4 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.conf import settings
-from django.core.files.storage import FileSystemStorage
 
-def simple_upload(request):
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
-        fs = FileSystemStorage()
-        filename = fs.save(myfile.name,myfile)
-        uploaded_file_url = fs.url(filename)
-        return render(request,'core/simple_upload')
+def index(request):
+    return HttpResponse("Hello, world. You're at the data index.")
--- datasite/datasite/urls.py
+++ datasite/datasite/urls.py
@@ -18,5 +18,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('data.urls')),
+    path('data/', include('data.urls')),
 ]
