047462c88edb4a2215714c1e629d0ea2bfd2b182
Ac65943 <ac65943@tamu.edu>
2020-03-24 21:10:54 -0500

    basic file upload

--- datasite/data/views.py
+++ datasite/data/views.py
@@ -1,4 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import UploadFileForm
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the data index.")
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST,request.FILES)
+        if form.is_valid():
+            handle_uploaded_file(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html',{'form':form})
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt','wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
--- datasite/datasite/settings.py
+++ datasite/datasite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'datasite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'data', 'templates', 'data'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
