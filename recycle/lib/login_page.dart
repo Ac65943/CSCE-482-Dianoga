a90a9340fc23cd2e7b833d95f568fc7b992e9260
Ac65943 <ac65943@tamu.edu>
2020-04-17 07:47:59 -0500

    adding firebase

--- recycle/lib/login_page.dart
+++ recycle/lib/login_page.dart
@@ -80,7 +80,7 @@ class FirstScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Scaffold(
-      body: Container(color: Colors.blue[100]),
+      body: Container(color: Colors.green[100]),
     );
   }
 }
