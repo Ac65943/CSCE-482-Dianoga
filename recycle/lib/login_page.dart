1478a7ac7d80366cb7f5f9dca917673b2e69b8f0
Ac65943 <ac65943@tamu.edu>
2020-04-21 15:24:06 -0500

    start over firebase

--- recycle/lib/login_page.dart
+++ recycle/lib/login_page.dart
@@ -92,68 +92,68 @@ class SignOutScreen extends StatelessWidget{
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             mainAxisSize: MainAxisSize.max,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundImage: NetworkImage(
-                  imageUrl,
-                ),
-                radius: 60,
-                backgroundColor: Colors.transparent,
-              ),
-              SizedBox(height: 40),
-              Text(
-                'NAME',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
-                ),
-              ),
-              Text(
-                name,
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Theme.of(context).primaryColorDark,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 20),
-              Text(
-                'EMAIL',
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black54,
-                ),
-              ),
-              Text(
-                email,
-                style: TextStyle(
-                  fontSize: 25,
-                  color: Theme.of(context).primaryColorDark,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: 40),
-              RaisedButton(
-                onPressed: (){
-                  signOutGoogle();
-                  Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context){return LoginPage();}), ModalRoute.withName('/'));
-                },
-                color: Theme.of(context).primaryColorDark,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Sign Out',
-                    style: TextStyle(fontSize:25,color:Colors.white),
-                  ),
-                ),
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(40),
-                ),
-              ),
-            ],
+          //   children: <Widget>[
+          //     CircleAvatar(
+          //       backgroundImage: NetworkImage(
+          //         imageUrl,
+          //       ),
+          //       radius: 60,
+          //       backgroundColor: Colors.transparent,
+          //     ),
+          //     SizedBox(height: 40),
+          //     Text(
+          //       'NAME',
+          //       style: TextStyle(
+          //         fontSize: 15,
+          //         fontWeight: FontWeight.bold,
+          //         color: Colors.black54,
+          //       ),
+          //     ),
+          //     Text(
+          //       name,
+          //       style: TextStyle(
+          //         fontSize: 25,
+          //         color: Theme.of(context).primaryColorDark,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //     SizedBox(height: 20),
+          //     Text(
+          //       'EMAIL',
+          //       style: TextStyle(
+          //         fontSize: 15,
+          //         fontWeight: FontWeight.bold,
+          //         color: Colors.black54,
+          //       ),
+          //     ),
+          //     Text(
+          //       email,
+          //       style: TextStyle(
+          //         fontSize: 25,
+          //         color: Theme.of(context).primaryColorDark,
+          //         fontWeight: FontWeight.bold,
+          //       ),
+          //     ),
+          //     SizedBox(height: 40),
+          //     RaisedButton(
+          //       onPressed: (){
+          //         signOutGoogle();
+          //         Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context){return LoginPage();}), ModalRoute.withName('/'));
+          //       },
+          //       color: Theme.of(context).primaryColorDark,
+          //       child: Padding(
+          //         padding: const EdgeInsets.all(8.0),
+          //         child: Text(
+          //           'Sign Out',
+          //           style: TextStyle(fontSize:25,color:Colors.white),
+          //         ),
+          //       ),
+          //       elevation: 5,
+          //       shape: RoundedRectangleBorder(
+          //         borderRadius: BorderRadius.circular(40),
+          //       ),
+          //     ),
+          //   ],
           ),
         ),
       ),
